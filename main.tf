489b34f3277e793d0a25fd758fe4d4d299ed6bad
zablon <zablonoigo51@gmail.com>
2025-03-01 15:25:41 +0300

    Create security group

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region=""
+    secret_key = ""
+    access_key = ""
+}
+
+resource "aws_security_group" "ec2-sg"{
+    name ="demo"
+    description = "Security group to allow traffic to EC2"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port=0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
