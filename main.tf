1519851aa664f87a329c2b20e829fa57eb1b2391
zablon <zablonoigo51@gmail.com>
2025-03-02 13:40:30 +0300

    Add lambda function

--- main.tf
+++ main.tf
@@ -35,3 +35,16 @@ resource "aws_sns_topic" "topic" {
     lambda_success_feedback_role_arn = ""
     lambda_success_feedback_sample_rate = "100"
 }
+resource "aws_sns_topic_subscription" "subscription" {
+    topic_arn=aws_sns_topic.topic.arn
+    protocol = "lambda"
+    endpoint=aws_lambda_function.lambda.arn
+}
+resource "aws_lambda_function" "lambda"{
+    filename = "lambda_fucntion.zip"
+    function_name="test"
+    role=""
+    handler="lambda_function.lambda_handler"
+    runtime="python3.9"
+    source_code_hash=filebase64("lambda_funtion.zip")   
+}
